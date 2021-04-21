4031853c91aa0a4744d5c3aac3848eda9725aa63
Raphaela Tamiette <ratamiette@gmail.com>
2021-04-21 15:14:36 -0300

    Adicionado testes unitários de mais cenarios.

--- ArtigoXUnitTestes/ArtigoXUnitTestes.UnitTests/Domain/Services/OperacaoFinanceiraServiceTransferirTests.cs
+++ ArtigoXUnitTestes/ArtigoXUnitTestes.UnitTests/Domain/Services/OperacaoFinanceiraServiceTransferirTests.cs
@@ -57,5 +57,59 @@ namespace ArtigoXUnitTestes.UnitTests.Domain.Services
             Assert.Equal(contaCorrenteDestinoSaldoEsperado, contaCorrenteDestino.Saldo);
             Assert.Equal(contaCorrenteDestinoLimiteEsperado, contaCorrenteDestino.Limite);
         }
+
+        [Fact]
+        public void ContaOrigemTemSaldoMasLimiteInsuficiente_ChamadoComContasEValoresValidos_RetornarFalhaENaoAlterarSaldosELimites()
+        {
+            const decimal valorTransacaoAcimaDoLimiteInicial = 20500;
+            const decimal contaCorrenteOrigemSaldoEsperado = 30000;
+            const decimal contaCorrenteOrigemLimiteEsperado = 20000;
+            const decimal contaCorrenteDestinoSaldoEsperado = 10000;
+            const decimal contaCorrenteDestinoLimiteEsperado = 20000;
+
+            // Assert
+            var contaCorrenteOrigem = ContaCorrenteFactory.GetContaOrigemValida();
+            var contaCorrenteDestino = ContaCorrenteFactory.ObterContaDestinoValida();
+
+            contaCorrenteOrigem.Creditar(20000);
+
+            var operacaoFinanceiraService = new OperacaoFinanceiraService();
+
+            // Act
+            var resultadoOperacao = operacaoFinanceiraService.Transferencia(contaCorrenteOrigem, contaCorrenteDestino, valorTransacaoAcimaDoLimiteInicial);
+
+            // Assert
+            Assert.False(resultadoOperacao);
+            Assert.Equal(contaCorrenteOrigemSaldoEsperado, contaCorrenteOrigem.Saldo);
+            Assert.Equal(contaCorrenteOrigemLimiteEsperado, contaCorrenteOrigem.Limite);
+            Assert.Equal(contaCorrenteDestinoSaldoEsperado, contaCorrenteDestino.Saldo);
+            Assert.Equal(contaCorrenteDestinoLimiteEsperado, contaCorrenteDestino.Limite);
+        }
+
+        [Fact]
+        public void ContaOrigemComLimiteESaldoInsuficientes_ChamadoComContasEValoresValidos_RetornarFalhaENaoAlterarSaldosELimites()
+        {
+            const decimal valorTransacaoAcimaDoSaldoELimiteInicial = 50000;
+            const decimal contaCorrenteOrigemSaldoEsperado = 10000;
+            const decimal contaCorrenteOrigemLimiteEsperado = 20000;
+            const decimal contaCorrenteDestinoSaldoEsperado = 10000;
+            const decimal contaCorrenteDestinoLimiteEsperado = 20000;
+
+            // Assert
+            var contaCorrenteOrigem = ContaCorrenteFactory.GetContaOrigemValida();
+            var contaCorrenteDestino = ContaCorrenteFactory.ObterContaDestinoValida();
+
+            var operacaoFinanceiraService = new OperacaoFinanceiraService();
+
+            // Act
+            var resultadoOperacao = operacaoFinanceiraService.Transferencia(contaCorrenteOrigem, contaCorrenteDestino, valorTransacaoAcimaDoSaldoELimiteInicial);
+
+            // Assert
+            Assert.False(resultadoOperacao);
+            Assert.Equal(contaCorrenteOrigemSaldoEsperado, contaCorrenteOrigem.Saldo);
+            Assert.Equal(contaCorrenteOrigemLimiteEsperado, contaCorrenteOrigem.Limite);
+            Assert.Equal(contaCorrenteDestinoSaldoEsperado, contaCorrenteDestino.Saldo);
+            Assert.Equal(contaCorrenteDestinoLimiteEsperado, contaCorrenteDestino.Limite);
+        }
     }
 }
